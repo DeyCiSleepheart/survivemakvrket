53d0d13a22120ada0092339b8d30dbf6511fc41c
SleepHeart <mortendalsgaard42@gmail.com>
2019-06-28 13:45:48 +0200

    first commit

---
+++ shareSurviveMakvrket/survivemakvrket.v0.1/js/actions_in_survive_makvrket.js
@@ -0,0 +1,21 @@
+
+//making survive makvrket more actionable
+
+
+let currentRoom = "start";
+let commands [“listen”, “pickup”];
+let inventory = [];
+
+
+listen
+ default:
+	$('#game_text').append("<p>You hear nothing of interest</p>”);
+
+
+//room —> areaInRoom —> item
+
+
+//you wake up on a strange place you know nothing
+
+//a: I… I am … I am John Snow?
+//b: no I do recall my name [input]
---
+++ shareSurviveMakvrket/survivemakvrket.v0.1/js/dontpanic.js
@@ -0,0 +1,20 @@
+// 
+// var frame = ["frame_1", "frame_2"];
+//
+// function goto();
+//
+// //function displayFrame() {
+//   document.getElementById("frame_2").addEventListener("click", function(goto){
+//     alert("hello world");
+//   });
+
+
+//}
+
+// if(getElementById('frame_2').style.display = 'block'){
+//   document.getElementById('frame_1').style.display = 'none';
+// }
+
+
+
+//on click (on the arrow) change the background image
